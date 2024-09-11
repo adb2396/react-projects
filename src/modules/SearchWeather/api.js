21984c285eb05941714010ca1035f3b7ecf08538
adb2396 <akshaybhamare10@gmail.com>
2024-09-11 21:55:09 +0530

    fix: search weather - added api key in env file

--- src/modules/SearchWeather/api.js
+++ src/modules/SearchWeather/api.js
@@ -8,7 +8,7 @@ export function useGetWeatherData() {
 	const fetchWeatherData = useCallback(async function (params) {
 		try {
 			const response = await fetch(
-				`https://api.openweathermap.org/data/2.5/weather?q=${params}&appid=eeaf5eeb260c6b5a4e8730802d80efbe`
+				`https://api.openweathermap.org/data/2.5/weather?q=${params}&appid=${process.env.REACT_APP_WEATHER_API_ID}`
 			)
 
 			const json = await response.json()
